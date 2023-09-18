d0a42125e764feabe10b8858362a802a702d559c
Rodrigo <rodrigohths@gmail.com>
2023-09-18 14:48:59 -0300

    fix: singleTest

--- aeds1/algoritmos-de-ordenacao/singleTest.java
+++ aeds1/algoritmos-de-ordenacao/singleTest.java
@@ -1,34 +1,31 @@
 public class singleTest {
   public static void main(String[] args) {
-    int size = 0, forSize = 1;
+    int size = 10000;
 
-    for (int i = 0; i < forSize; i++) {
-      size += 100000;
-      System.out.println("Tamanho do array: " + size);
-      int[] a = sorting.generateRandomArray(size);
-      long startTime = System.currentTimeMillis();
-      sorting.selectionSort(a);
-      long endTime = System.currentTimeMillis();
-      System.out.println("(Selection Sort): " + (endTime - startTime) + " milissegundos");
+    System.out.println("Tamanho do array: " + size);
+    int[] a = sorting.generateRandomArray(size);
+    long startTime = System.currentTimeMillis();
+    sorting.selectionSort(a);
+    long endTime = System.currentTimeMillis();
+    System.out.println("(Selection Sort): " + (endTime - startTime) + " milissegundos");
 
-      int[] b = sorting.generateRandomArray(size);
-      startTime = System.currentTimeMillis();
-      sorting.insertionSort(b);
-      endTime = System.currentTimeMillis();
-      System.out.println("(Insertion Sort): " + (endTime - startTime) + " milissegundos");
+    int[] b = sorting.generateRandomArray(size);
+    startTime = System.currentTimeMillis();
+    sorting.insertionSort(b);
+    endTime = System.currentTimeMillis();
+    System.out.println("(Insertion Sort): " + (endTime - startTime) + " milissegundos");
 
-      int[] c = sorting.generateRandomArray(size);
-      startTime = System.currentTimeMillis();
-      sorting.bubbleSort(c);
-      endTime = System.currentTimeMillis();
-      System.out.println("(Bubble Sort): " + (endTime - startTime) + " milissegundos");
+    int[] c = sorting.generateRandomArray(size);
+    startTime = System.currentTimeMillis();
+    sorting.bubbleSort(c);
+    endTime = System.currentTimeMillis();
+    System.out.println("(Bubble Sort): " + (endTime - startTime) + " milissegundos");
 
-      int[] d = sorting.generateRandomArray(size);
-      startTime = System.currentTimeMillis();
-      sorting.mergeSort(d, 0, d.length - 1);
-      endTime = System.currentTimeMillis();
-      System.out.println("(Merge Sort): " + (endTime - startTime) + " milissegundos\n");
+    int[] d = sorting.generateRandomArray(size);
+    startTime = System.currentTimeMillis();
+    sorting.mergeSort(d, 0, d.length - 1);
+    endTime = System.currentTimeMillis();
+    System.out.println("(Merge Sort): " + (endTime - startTime) + " milissegundos\n");
 
-    }
   }
 }
