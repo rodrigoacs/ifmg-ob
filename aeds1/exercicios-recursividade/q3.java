97c6a1fdd74a4ca0e1cbc4ded2ca748c86534d5e
Rodrigo <rodrigohths@gmail.com>
2023-09-26 16:14:41 -0300

    questao 3 com operador ternario

--- aeds1/exercicios-recursividade/q3.java
+++ aeds1/exercicios-recursividade/q3.java
@@ -1,18 +1,15 @@
 // 3 - Escreva uma função recursiva que recebe um número e verifica se este é um primo ou não. 
 public class q3 {
   public static void main(String[] args) {
-    final int INITIAL_DIVISOR = 2;
-    System.out.println(isPrime(144, INITIAL_DIVISOR));
+    for (int i = 1; i <= 100; i++) {
+      if (isPrime(i, 2)) {
+        System.out.println(i);
+      }
+    }
   }
 
-  public static boolean isPrime(int n, int divisor) {
-    if (n <= 1 || n % divisor == 0) {
-      return false;
-    }
-    if (divisor * divisor > n) {
-      return true;
-    }
-    return isPrime(n, divisor + 1);
+  public static boolean isPrime(int n, int index) {
+    return (index >= n) ? true : (n % index == 0) ? false : isPrime(n, index + 1);
   }
 
 }
